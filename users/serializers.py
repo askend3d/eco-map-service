f921dc2fb84a14d120d9ab4f02e8f384aedd9f27
Quatry <stop.mean1@gmail.com>
2025-10-30 21:16:24 +0300

    short register

--- users/serializers.py
+++ users/serializers.py
@@ -63,12 +63,10 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'role']
+        fields = ['username', 'email', 'password', 'role']
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
