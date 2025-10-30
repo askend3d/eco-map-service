4db0255b71b5b1b1abc3bdf10f1da5066b75b52e
Quatry <stop.mean1@gmail.com>
2025-10-30 12:16:51 +0300

    комменты по points/<id>/comments

--- pollution/views.py
+++ pollution/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import PollutionPoint, Comment
 from .serializers import PollutionPointSerializer, CommentSerializer
 
@@ -16,6 +19,28 @@ class PollutionPointViewSet(viewsets.ModelViewSet):
         if user.role == 'ngo' or user.is_staff:
             return PollutionPoint.objects.all()
         return PollutionPoint.objects.filter(status__in=['new', 'in_progress'])
+
+    @action(detail=True, methods=['get', 'post'], permission_classes=[permissions.IsAuthenticated])
+    def comments(self, request, pk=None):
+        """
+        GET: список комментариев к точке
+        POST: создать комментарий для точки
+        """
+        try:
+            point = PollutionPoint.objects.get(pk=pk)
+        except PollutionPoint.DoesNotExist:
+            return Response({"detail": "PollutionPoint not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        if request.method == 'GET':
+            comments = point.comments.all().order_by('-created_at')
+            serializer = CommentSerializer(comments, many=True)
+            return Response(serializer.data)
+
+        elif request.method == 'POST':
+            serializer = CommentSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(author=request.user, point=point)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
